81741dba60adc102a19cbbea5273e18e0f92ea01
Quadri <kazeemquadri75@gmail.com>
2023-10-03 14:11:33 +0100

    Route-Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import "./App.css";
+import Router from "./Router";
 
 function App() {
   return (
-    <>
-      <h1
-        className="header"
-        style={{ backgroundColor: "var(--primary-color)" }}
-      >
-        Hello
-      </h1>
-      <h2>world</h2>
-    </>
+    <Router/>
+    
   );
 }
 
