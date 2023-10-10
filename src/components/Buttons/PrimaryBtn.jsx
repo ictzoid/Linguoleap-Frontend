4bf2dc5fee50499b1ff7e28eed9b87e014cf7ee3
Quadri <kazeemquadri75@gmail.com>
2023-10-10 22:03:35 +0100

    login

--- src/components/Buttons/PrimaryBtn.jsx
+++ src/components/Buttons/PrimaryBtn.jsx
@@ -1,13 +1,13 @@
-import {propTypes} from "prop-types"
+// import {propTypes} from "prop-types"
   const PrimaryBtn = ({text}) => {
   return (
 
     <button>{text ? text : "i'm a button"}</button>
     
   )
-}
-PrimaryBtn.propTypes={  
-    text: propTypes.string.isRequired  // string type is required and can't be null or undefined
+// }
+// PrimaryBtn.propTypes={  
+//     text: propTypes.string.isRequired  // string type is required and can't be null or undefined
 }
 
 export default PrimaryBtn
